17b5bd2d873a25782757be329cc8f2a7a0ce0b71
rain <20520@126.com>
2022-06-16 13:53:30 +0800

    update

---
+++ convey/ProcessInfo.h
@@ -0,0 +1,60 @@
+#ifndef CONVEY_PROCESSINFO_H
+#define CONVEY_PROCESSINFO_H
+
+#include <sys/types.h>
+#include <vector>
+#include "../convey/StringPiece.h"
+#include "../convey/Timestamp.h"
+#include "../convey/Types.h"
+
+namespace convey
+{
+namespace ProcessInfo
+{
+pid_t pid();
+string pidString();
+uid_t uid();
+string username();
+uid_t euid();
+Timestamp startTime();
+int clockTicksPerSecond();
+int pageSize();
+bool isDebugBuild();  // constexpr
+
+string hostname();
+string procname();
+StringPiece procname(const string &stat);
+
+/// read /proc/self/status
+string procStatus();
+
+/// read /proc/self/stat
+string procStat();
+
+/// read /proc/self/task/tid/stat
+string threadStat();
+
+/// readlink /proc/self/exe
+string exePath();
+
+int openedFiles();
+int maxOpenFiles();
+
+struct CpuTime
+{
+  double userSeconds;
+  double systemSeconds;
+
+  CpuTime() : userSeconds(0.0), systemSeconds(0.0) {}
+
+  double total() const { return userSeconds + systemSeconds; }
+};
+CpuTime cpuTime();
+
+int numThreads();
+std::vector<pid_t> threads();
+}  // namespace ProcessInfo
+
+}  // namespace convey
+
+#endif
