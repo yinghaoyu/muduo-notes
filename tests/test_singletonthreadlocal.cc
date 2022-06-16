e592190efdfb7e917edb5600c4beabeb0ec4e044
rain <20520@126.com>
2022-06-16 14:38:51 +0800

    add singletonthreadlocal

---
+++ tests/test_singletonthreadlocal.cc
@@ -0,0 +1,49 @@
+#include "../convey/CurrentThread.h"
+#include "../convey/Singleton.h"
+#include "../convey/Thread.h"
+#include "../convey/ThreadLocal.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+class Test : convey::noncopyable
+{
+ public:
+  Test() { printf("tid=%d, constructing %p\n", convey::CurrentThread::tid(), this); }
+
+  ~Test() { printf("tid=%d, destructing %p %s\n", convey::CurrentThread::tid(), this, name_.c_str()); }
+
+  const convey::string &name() const { return name_; }
+  void setName(const convey::string &n) { name_ = n; }
+
+ private:
+  convey::string name_;
+};
+
+#define STL convey::Singleton<convey::ThreadLocal<Test> >::instance().value()
+
+void print()
+{
+  printf("tid=%d, %p name=%s\n", convey::CurrentThread::tid(), &STL, STL.name().c_str());
+}
+
+void threadFunc(const char *changeTo)
+{
+  print();
+  STL.setName(changeTo);
+  sleep(1);
+  print();
+}
+
+int main()
+{
+  STL.setName("main one");
+  convey::Thread t1(std::bind(threadFunc, "thread1"));
+  convey::Thread t2(std::bind(threadFunc, "thread2"));
+  t1.start();
+  t2.start();
+  t1.join();
+  print();
+  t2.join();
+  pthread_exit(0);
+}
