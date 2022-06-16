ebdb57578ad2660861c748e10117fe8281f0fbaf
rain <20520@126.com>
2022-06-16 14:16:16 +0800

    add test_processinfo

---
+++ tests/test_processinfo.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../convey/ProcessInfo.h"
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+int main()
+{
+  printf("pid = %d\n", convey::ProcessInfo::pid());
+  printf("uid = %d\n", convey::ProcessInfo::uid());
+  printf("euid = %d\n", convey::ProcessInfo::euid());
+  printf("start time = %s\n", convey::ProcessInfo::startTime().toFormattedString().c_str());
+  printf("hostname = %s\n", convey::ProcessInfo::hostname().c_str());
+  printf("opened files = %d\n", convey::ProcessInfo::openedFiles());
+  printf("threads = %zd\n", convey::ProcessInfo::threads().size());
+  printf("num threads = %d\n", convey::ProcessInfo::numThreads());
+  printf("status = %s\n", convey::ProcessInfo::procStatus().c_str());
+}
